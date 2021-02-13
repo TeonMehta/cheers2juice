9cbcf0596cd0049df9298333c64fd2bd0dd4f2f4
TeonMehta <teonmehta@gmail.com>
2021-02-13 18:47:37 -0500

    first commit

---
+++ ecommerce-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '11b29df87c9995a39796d825153abb78'),
+    },
+  },
+});
---
+++ ecommerce-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2080f832-00d1-4cef-97b3-f9b70412d7ab'
+};
